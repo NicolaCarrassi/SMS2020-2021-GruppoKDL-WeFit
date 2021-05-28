c6d193f29b0370a82293f27e204715b7231d200f
fraalber <francesco.albergo7@gmail.com>
2021-05-28 18:39:05 +0200

    Aggiunto Drawer per il cliente (design e funzionalitá), aggiunti colori, temi e icone necessari, aggiunti fragment per opzioni drawer, aggiunte stringhe necessarie ita e eng,

--- app/src/main/java/it/uniba/di/sms2021/gruppodkl/wefit/MainActivityUser.java
+++ app/src/main/java/it/uniba/di/sms2021/gruppodkl/wefit/MainActivityUser.java
@@ -1,20 +1,39 @@
 package it.uniba.di.sms2021.gruppodkl.wefit;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MenuCompat;
+import androidx.core.view.MenuItemCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.text.Layout;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
+import it.uniba.di.sms2021.gruppodkl.wefit.fragment.CoachProfileFragment;
 import it.uniba.di.sms2021.gruppodkl.wefit.fragment.DietFragment;
 import it.uniba.di.sms2021.gruppodkl.wefit.fragment.HomeFragmentUser;
+
+import it.uniba.di.sms2021.gruppodkl.wefit.fragment.NotificationsFragment;
+import it.uniba.di.sms2021.gruppodkl.wefit.fragment.ProfileFragment;
+import it.uniba.di.sms2021.gruppodkl.wefit.fragment.ProgressFragment;
+import it.uniba.di.sms2021.gruppodkl.wefit.fragment.SettingsFragment;
+import it.uniba.di.sms2021.gruppodkl.wefit.fragment.TermsFragment;
 import it.uniba.di.sms2021.gruppodkl.wefit.fragment.TrainingFragment;
 
 public class MainActivityUser extends AppCompatActivity {
 
     private BottomNavigationView mBottomNavigation;
+    private NavigationView mNavigationView;
+    private DrawerLayout mDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +47,7 @@ public class MainActivityUser extends AppCompatActivity {
 
         bind();
         setListener();
+        setNavigationView();
 
     }
 
@@ -86,5 +106,52 @@ public class MainActivityUser extends AppCompatActivity {
         });
     }
 
+    private void setNavigationView(){
+        mDrawer = findViewById(R.id.drawer_layout);
+        mNavigationView = findViewById(R.id.navigation);
+        mNavigationView.inflateMenu(R.menu.menu_drawer_cliente);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                showFragment(item);
+                Log.d("DRAWER","Selezionato nel drawer: " + item);
+                mDrawer.closeDrawer(mNavigationView);
+                return false;
+            }
+        });
+    }
+
+    private void showFragment(final MenuItem item){
+        final int itemId = item.getItemId();
+        Fragment nextFragment = new Fragment();
+
+        switch(itemId){
+            case R.id.profile_item:
+                nextFragment = new ProfileFragment();
+                break;
+            case R.id.coach_item:
+                nextFragment = new CoachProfileFragment();
+                break;
+            case R.id.notifications_item:
+                nextFragment = new NotificationsFragment();
+                break;
+            case R.id.progress_item:
+                nextFragment = new ProgressFragment();
+                break;
+            case R.id.settings:
+                nextFragment = new SettingsFragment();
+                break;
+            case R.id.terms_item:
+                nextFragment = new TermsFragment();
+                break;
+            case R.id.logout_item:
+                //TODO
+                break;
+            default:
+                throw new IllegalArgumentException("Nessun Fragment per item selezionato");
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.anchor_point, nextFragment).commit();
+
+    }
 
 }

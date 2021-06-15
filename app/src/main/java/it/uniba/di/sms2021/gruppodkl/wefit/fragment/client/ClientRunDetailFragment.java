6533beb889a08c43f8da018f60fbbf58b18f22d1
KdLSosa <gabriele.colasuonno@gmail.com>
2021-06-15 10:24:09 +0200

    Aggiornato layout dei dettagli delle corse effettuate in passato.

--- app/src/main/java/it/uniba/di/sms2021/gruppodkl/wefit/fragment/client/ClientRunDetailFragment.java
+++ app/src/main/java/it/uniba/di/sms2021/gruppodkl/wefit/fragment/client/ClientRunDetailFragment.java
@@ -2,6 +2,8 @@ package it.uniba.di.sms2021.gruppodkl.wefit.fragment.client;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,8 +12,20 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.List;
+
 import it.uniba.di.sms2021.gruppodkl.wefit.R;
 import it.uniba.di.sms2021.gruppodkl.wefit.WeFitApplication;
+import it.uniba.di.sms2021.gruppodkl.wefit.model.MyLocation;
 import it.uniba.di.sms2021.gruppodkl.wefit.model.Run;
 
 /**
@@ -20,12 +34,13 @@ import it.uniba.di.sms2021.gruppodkl.wefit.model.Run;
  * create an instance of this fragment.
  */
 // TODO Inserire grafico della corsa
-public class ClientRunDetailFragment extends Fragment {
+public class ClientRunDetailFragment extends Fragment implements OnMapReadyCallback {
     public static final String TAG = ClientRunDetailFragment.class.getSimpleName();
 
     private static final String RUN_KEY = "run";
 
     private Run mRun;
+    private GoogleMap mMap;
     private TextView mRunDate;
     private TextView mRunTime;
     private TextView mRunKcal;
@@ -80,16 +95,70 @@ public class ClientRunDetailFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+    }
+
     /**
      * Il metodo permette di impostare i valori della corsa
      */
     private void setValues(){
         mRunDate.setText(mRun.date);
-        mRunTime.setText(mRun.elapsedTime);
         mRunKcal.setText(mRun.convertKcal());
-        mRunDistance.setText(mRun.convertRunDistance());
-        mRunSpeed.setText(mRun.convertAvgSpeed());
     }
 
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        mMap.getUiSettings().setMapToolbarEnabled(false);
+        showLastRun(mRun);
+    }
+
+    public void showLastRun(Run run) {
+        LatLng startPoint = new LatLng(run.locationList.get(0).getLatitude(),run.locationList.get(0).getLongitude());
+        LatLng endPoint = new LatLng(run.locationList.get(run.locationList.size()-1).getLatitude(),run.locationList.get(run.locationList.size()-1).getLongitude());
+        mMap.addMarker(new MarkerOptions().position(startPoint).title(getString(R.string.starting_point)));
+        mMap.addMarker(new MarkerOptions().position(endPoint).title(getString(R.string.stop_point)));
+        for(int i=0;i<(run.locationList.size()-1);i++){
+            mMap.addPolyline(new PolylineOptions()
+                    .color(getResources().getColor(R.color.blue, getActivity().getTheme()))
+                    .add(new LatLng(run.locationList.get(i).getLatitude(),run.locationList.get(i).getLongitude()),
+                            new LatLng(run.locationList.get(i+1).getLatitude(),run.locationList.get(i+1).getLongitude())));
+        }
+        centerCamera(run.locationList);
+        mRunSpeed.setText(run.convertAvgSpeed());
+        mRunDistance.setText(run.convertRunDistance());
+        mRunTime.setText(run.convertTime());
+    }
+
+    public void centerCamera(List<MyLocation> locationList){
+
+        LatLng southwest = new LatLng(locationList.get(0).getLatitude(),locationList.get(0).getLongitude());
+        LatLng northeast = new LatLng(locationList.get(0).getLatitude(),locationList.get(0).getLongitude());
+        LatLngBounds myPosition;
+
+        for(int i=1;i<locationList.size();i++){
+            if(locationList.get(i).getLatitude()+locationList.get(i).getLongitude() > northeast.latitude + northeast.longitude){
+                northeast = new LatLng(locationList.get(i).getLatitude(),locationList.get(i).getLongitude());
+            } else if(locationList.get(i).getLatitude() + locationList.get(i).getLongitude() < southwest.latitude + southwest.longitude){
+                southwest = new LatLng(locationList.get(i).getLatitude(),locationList.get(i).getLongitude());
+            }
+        }
+
+        if(northeast.latitude>southwest.latitude) {
+            myPosition = new LatLngBounds(southwest, northeast);
+            mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(myPosition, 150));
+        } else {
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(southwest.latitude,southwest.longitude),16));
+        }
+    }
 
 }
